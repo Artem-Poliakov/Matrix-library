3a2552a7e2e4549118d4d12c83e10df4239a6727
Поляков Артём Александрович <aapoliakov@edu.hse.ru>
2024-10-22 22:55:49 +0300

    Created tests directory

--- source/main.cpp
+++ source/main.cpp
@@ -2,13 +2,5 @@
 #include <iostream>
 
 int main() {
-    const linalg::Matrix mat1 = {{1, 0, 8}, {9, -4, 9}, {1, -5, 0}};
-    linalg::Matrix mat2 = {{-5, 1, 7}, {2, -7, 1}, {-3, -5, 9}};
-
-    std::cout << mat1[0][0] << std::endl;
-    std::cout << mat2[0][0] << std::endl;
-//  std::cout << (mat1[0][0] = 0) << std::endl; // forbidden 
-    std::cout << (mat2[0][0] = 0) << std::endl;
-    std::cout << mat1 << std::endl;
-    std::cout << mat2 << std::endl;
+    linalg::Matrix mat;
 }
